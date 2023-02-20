51d9f640280f140ef26358936a54a88e3474e818
Owolabi Celina <108185066+OwolabiCelina@users.noreply.github.com>
2023-02-20 11:29:36 +0100

    social-proof

---
+++ script/script.js
@@ -0,0 +1,86 @@
+// console.log('Hello world')
+
+// var z = 10
+// var x = 15
+// var y = z + x
+
+// console.log(y)
+
+// const cars = {"Saab", "Volvo", "BMW"};
+// cars[0] = "Toyota";
+// cars.push("Audi");
+
+// console.log(cars);
+
+// let cars = ["Benz", "Camry", "Mercedez"];
+// console.log(cars);
+// cars[1] = "Bugatti";
+// cars.push("Ferrari");
+
+// console.log(cars);
+
+
+// var cars = ["Benz", "Toyota"];
+// cars[0] = "Camry";
+// cars.push("Motorola");
+
+// console.log('Omobanke Celina')
+
+// name = 'Omobanke'
+// console.log(name)
+
+// sentence = 'Its been a while Celina, how have you been ? I hope the family is doing great?'
+
+// console.log(sentence)
+
+//  // operators
+// fruit = prompt('what is your favorite fruit ?')
+
+// console.log(fruit)
+
+
+// function myFunction(p1, p2, p3){
+//     return p1 / p2 + p3;
+// }
+// console.log(myFunction(20, 4, 2))
+
+
+// food = Number(prompt('How much was the food ?'));
+// tipPercentage = Number(prompt('tip %') / 100);
+// tipAmount = food * tipPercentage
+// total = food + tipAmount
+
+// console.log(tipAmount); 
+// console.log(total);
+// alert(tipAmount)
+
+// let userInput = prompt("What is your name");
+
+// console.log(`My name is ${userInput}`);
+
+
+// let schoolName = "Unilag";
+// console.log(`The name of my school is ${schoolName} located in Akoka, Yaba `);
+
+// let age = 27;
+// console.log(`Im currently ${age}`);
+
+// let color = "Black";
+// console.log(`My best color is ${color}`);
+
+// user input
+
+// console.log("My name is banke");
+// num1 = 2;
+// num1 == 5 ? true : false;
+
+// num = 10;
+// if(num == 5){
+//     console.log(true);
+// }else {
+//     console.log(false);
+// }
+
+// let school = "Unilag";
+
+
